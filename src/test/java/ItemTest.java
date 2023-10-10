d18c171cd09491fdd4460bebdbfe522e0627a8f7
MagneticMojo <forsberg.bjorn.a@gmail.com>
2023-10-10 13:00:35 +0200

    Equivalence partitions eval started for Item class

--- src/test/java/ItemTest.java
+++ src/test/java/ItemTest.java
@@ -6,13 +6,29 @@ class ItemTest {
 
     // Price
     @Test
-    void testCreatesItem() {
+    void testCreatesItemWithName() {
         Item i = new Item("AN ITEM", 1000);
-        assertEquals(1000, i.getPrice());
         assertEquals("AN ITEM", i.getName());
     }
 
     // Name
+    @Test
+    void testCreatesItemWithCorrectPrice() {
+        Item i = new Item("AN ITEM", 1000);
+        assertEquals(1000, i.getPrice());
+    }
+
+    // (IN) Null : null
+    // (IN) Empty : ""
+    // (IN) Blank : "   "
+    // (IN) Non-alphanumeric characters include : ==> FAIL
+    // (IN) str.length() > 20
+    // (V) Trailing spaces : "    AN ITEM      " => TRIMS
+    // (V) Space in between:  "AN     ITEM" => "AN ITEM"
+    // (V) Trailing && in between : "    AN     ITEM    "
+    // (V)
+
+
     // EAN
     // Item category
 
