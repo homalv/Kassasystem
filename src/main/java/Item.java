4f69ce28e47635d055d68c691aec94386ec50cdc
MagneticMojo <forsberg.bjorn.a@gmail.com>
2023-10-10 11:28:01 +0200

    Money class last version

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,2 +1,18 @@
 public class Item {
+
+    private final Money price;
+    private final String name;
+
+    public Item(String name, Money price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/ItemTest.java
+++ src/test/java/ItemTest.java
@@ -1,7 +1,13 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ItemTest {
 
+    // Price
+    // Name
     // EAN
+    // Item category
+
 
 }
