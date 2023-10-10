57a2e8333a20050c12ee03630187c259ca6ccd4a
MagneticMojo <forsberg.bjorn.a@gmail.com>
2023-10-10 11:33:31 +0200

    Item class first test

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,14 +1,14 @@
 public class Item {
 
-    private final Money price;
+    private final long price;
     private final String name;
 
-    public Item(String name, Money price) {
+    public Item(String name, long price) {
         this.name = name;
         this.price = price;
     }
 
-    public Money getPrice() {
+    public long getPrice() {
         return price;
     }
 
--- src/test/java/ItemTest.java
+++ src/test/java/ItemTest.java
@@ -5,9 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class ItemTest {
 
     // Price
+    @Test
+    void testCreatesItem() {
+        Item i = new Item("AN ITEM", 1000);
+        assertEquals(1000, i.getPrice());
+        assertEquals("AN ITEM", i.getName());
+    }
+
     // Name
     // EAN
     // Item category
+
+
 
 
 }
