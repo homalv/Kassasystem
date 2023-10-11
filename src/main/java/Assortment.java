1c0315f5a998e411f88fd93fa67c70835aecac80
MagneticMojo <forsberg.bjorn.a@gmail.com>
2023-10-11 17:20:16 +0200

    add updated Assortment and AssortmentTest

--- src/main/java/Assortment.java
+++ src/main/java/Assortment.java
@@ -1,8 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class Assortment {
 
-    // Get from file
+    // Integer should be replaced by EAN???
+    private Map<Long, Item> items;
 
-    // Map<EAN, Item> products
+    public Assortment(String csvResourcePath)  {
+        if (!csvResourcePath.endsWith(".csv")) {
+            throw new IllegalArgumentException("Provided resource path is not a CSV file: " + csvResourcePath);
+        }
 
+        items = new HashMap<>();
 
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(Objects.requireNonNull(getClass().getResourceAsStream(csvResourcePath),
+                        "Resource not found: " + csvResourcePath)))) {
+
+            reader.readLine();
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+
+                if (parts.length != 3) {
+                    throw new ArrayIndexOutOfBoundsException("CSV file format is incorrect. Ensure each line has the correct number of fields");
+                }
+
+                Long ean = Long.parseLong(parts[0]);
+                Item item = new Item(parts[1], Long.parseLong(parts[2]));
+                items.put(ean, item);
+            }
+        } catch (IOException e) {
+            System.err.println("IOException caught: Error reading the CSV file: " + e.getMessage());
+            throw new RuntimeException("IOException caught: Error reading the CSV file");
+        } catch (NumberFormatException e) {
+            System.err.println("Error parsing a number from the CSV file: " + e.getMessage());
+            throw new NumberFormatException("Error parsing a number from the CSV file");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("CSV file format is incorrect. Ensure each line has the correct number of fields: " + e.getMessage());
+            throw new ArrayIndexOutOfBoundsException("CSV file format is incorrect. Ensure each line has the correct number of fields");
+        }
+
+    }
+
+    public int getAssortmentSize() {
+        return items.size();
+    }
+
+    public boolean contains(long ean) {
+        return items.containsKey(ean);
+    }
 }
--- src/test/java/AssortmentTest.java
+++ src/test/java/AssortmentTest.java
@@ -1,6 +1,46 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AssortmentTest {
 
+    private static final String ASSORTMENT_RESOURCE_PATH = "/test-assortment.csv";
+    private static final String NON_CSV_FILE_PATH = "/items.txt";
+    private static final String INVALID_PATH = "/invalid/invalid.csv";
+    private static final String NON_NUMERICAL_STRING_IN_CSV_PATH = "/non-numerical-item-string.csv";
+    private static final String OUT_OF_BOUNDS_CSV_PATH = "/oob.csv";
+    private static final long PINEAPPLE_EAN = 1234567890017L;
+
+    @Test
+    void ctrThrowsForNonCsvFile() {
+        assertThrows(IllegalArgumentException.class, () -> new Assortment(NON_CSV_FILE_PATH));
+    }
+
+    @Test
+    void ctrThrowsForInvalidResourcePath() {
+        assertThrows(NullPointerException.class, () -> new Assortment(INVALID_PATH));
+    }
+
+    @Test
+    void ctrThrowsForNonNumericalString() {
+        assertThrows(NumberFormatException.class, () -> new Assortment(NON_NUMERICAL_STRING_IN_CSV_PATH));
+    }
+
+    @Test
+    void ctrThrowsForInvalidCSVFormat() {
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> new Assortment(OUT_OF_BOUNDS_CSV_PATH));
+    }
+
+    @Test
+    void ctrPopulatesAssortment() {
+        Assortment assortment = new Assortment(ASSORTMENT_RESOURCE_PATH);
+        assertEquals(50, assortment.getAssortmentSize());
+    }
+
+    @Test
+    void testAssortmentContainsItem() {
+        Assortment assortment = new Assortment(ASSORTMENT_RESOURCE_PATH);
+        assertTrue(assortment.contains(PINEAPPLE_EAN));
+    }
 
 }
