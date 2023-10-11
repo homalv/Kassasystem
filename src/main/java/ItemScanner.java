7a6e8091243dd83fe65f04001fb73732aa189cc0
nidorbRotkiv <viktor.karl.brodin.1@gmail.com>
2023-10-11 13:29:28 +0200

    First version of ItemScanner and tests

--- src/main/java/ItemScanner.java
+++ src/main/java/ItemScanner.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class ItemScanner {
+import java.util.HashMap;
+
+public class ItemScanner {
+    private final HashMap<String, Item> items = new HashMap<>();
+
+    public ItemScanner() {
+        // TEMP SOLUTION (SHOULD PROBABLY BE MOCKED OR SOMETHING)
+        items.put("1234567890123", new Item("Milk", 10000));
+        items.put("9876543210987", new Item("Bread", 5000));
+    }
+
+    public Item findItem(String ean) {
+        return items.get(ean);
+    }
+
 }
