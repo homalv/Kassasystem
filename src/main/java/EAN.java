c5123ebc06970917b0fe4ab5292579cda77c8807
adsa4926 <adsal92@hotmail.com>
2023-10-13 09:48:02 +0200

    Refactored EAN class + added checkdigit function

--- src/main/java/EAN.java
+++ src/main/java/EAN.java
@@ -2,62 +2,93 @@ public class EAN {
     private String EANNumber = "";
     private String DEFAULT_COUNTRY_PREFIX = "012";
     private String DEFAULT_MANUFACTURER_ID = "34567";
-    
-    public EAN(String countryPrefix, String manufacturerDigits, String productDigits){
-        isCountryPrefixValid(countryPrefix);
-        isManufacturerDigitsValid(manufacturerDigits);
-        isProductDigitsValid(productDigits);
-        this.EANNumber += countryPrefix;
-        this.EANNumber += manufacturerDigits;
-        this.EANNumber += productDigits;
+
+    public EAN(String countryPrefix, String manufacturerDigits, String productDigits) {
+        this.EANNumber += isCountryPrefixValid(countryPrefix);
+        this.EANNumber += isManufacturerDigitsValid(manufacturerDigits);
+        this.EANNumber += isProductDigitsValid(productDigits);
+        this.EANNumber += calculateCheckDigit();
     }
-    public EAN(String productDigits){
+
+    public EAN(String productDigits) {
         this.EANNumber += DEFAULT_COUNTRY_PREFIX;
-        this.EANNumber += DEFAULT_MANUFACTURER_ID; 
+        this.EANNumber += DEFAULT_MANUFACTURER_ID;
         this.EANNumber += productDigits;
+        this.EANNumber += calculateCheckDigit();
     }
+
+    public EAN(Long completeEAN) {
+        if (completeEAN.equals(null)) {
+            throw new NullPointerException();
+        }
+        String completeEANString = Long.toString(completeEAN);
+        if (completeEANString.length() != 13) {
+            throw new IllegalArgumentException();
+        }
+        this.EANNumber = completeEANString;
+    }
+
     public String getEANNumber() {
         return EANNumber;
     }
-    private void isCountryPrefixValid(String countryPrefix){
-        if (countryPrefix.length() != 3){
-            throw new IllegalArgumentException("Country prefix needs to be 3 digits");
-        }
-        if (countryPrefix.equals(null)){
-            throw new NullPointerException("Country prefix can't be null");
-        }
-        for (char c : countryPrefix.toCharArray()){
-            if(!Character.isDigit(c)){
-                throw new IllegalArgumentException("Only digits are accepted, 0-9");
+
+    private int calculateCheckDigit() {
+        char[] EANArray = EANNumber.toCharArray();
+        int checkDigitSum = 0;
+        for (int i = 0; i < EANArray.length; i++) {
+            int digit = Character.getNumericValue(EANArray[i]);
+            if ((i) % 2 == 0) {
+                checkDigitSum += digit * 3;
+            } else {
+                checkDigitSum += digit;
             }
         }
-    
-    }
-     private void isManufacturerDigitsValid(String manufacturerDigits){
-        if (manufacturerDigits.length() != 5){
-            throw new IllegalArgumentException("Manufacturer digits needs to be 5 digits");
-        }
-        if (manufacturerDigits.equals(null)){
-            throw new NullPointerException("Manufacturer digits can't be null");
-        }
-        for (char c : manufacturerDigits.toCharArray()){
-            if(!Character.isDigit(c)){
-                throw new IllegalArgumentException("Only digits are accepted, 0-9");
-            }
-        }
+        return 10 - (checkDigitSum % 10);
     }
 
-     private void isProductDigitsValid(String productDigits){
-        if (productDigits.length() != 4){
-            throw new IllegalArgumentException("Manufacturer digits needs to be 4 digits");
+    private String isCountryPrefixValid(String countryPrefix) {
+        if (countryPrefix.length() != 3) {
+            throw new IllegalArgumentException("Country prefix needs to be 3 digits");
         }
-        if (productDigits.equals(null)){
-            throw new NullPointerException("Manufacturer digits can't be null");
+        if (countryPrefix.equals(null)) {
+            throw new NullPointerException("Country prefix can't be null");
         }
-        for (char c : productDigits.toCharArray()){
-            if(!Character.isDigit(c)){
+        for (char c : countryPrefix.toCharArray()) {
+            if (!Character.isDigit(c)) {
                 throw new IllegalArgumentException("Only digits are accepted, 0-9");
             }
         }
+        return countryPrefix;
+
+    }
+
+    private String isManufacturerDigitsValid(String manufacturerDigits) {
+        if (manufacturerDigits.length() != 5) {
+            throw new IllegalArgumentException("Manufacturer digits needs to be 5 digits");
+        }
+        if (manufacturerDigits.equals(null)) {
+            throw new NullPointerException("Manufacturer digits can't be null");
+        }
+        for (char c : manufacturerDigits.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException("Only digits are accepted, 0-9");
+            }
+        }
+        return manufacturerDigits;
+    }
+
+    private String isProductDigitsValid(String productDigits) {
+        if (productDigits.length() != 4) {
+            throw new IllegalArgumentException("Manufacturer digits needs to be 4 digits");
+        }
+        if (productDigits.equals(null)) {
+            throw new NullPointerException("Manufacturer digits can't be null");
+        }
+        for (char c : productDigits.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException("Only digits are accepted, 0-9");
+            }
+        }
+        return productDigits;
     }
 }
