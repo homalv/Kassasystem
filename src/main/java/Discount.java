bc957b2cbf28713ad913d7661a12158a5d4d2b6d
nidorbRotkiv <viktor.karl.brodin.1@gmail.com>
2023-10-16 11:54:22 +0200

    Added

--- src/main/java/Discount.java
+++ src/main/java/Discount.java
@@ -2,7 +2,7 @@ public enum Discount {
     TEN(10),
     TWENTY(20),
     TWENTYFIVE(25),
-    FIFTY(50)
+    FIFTY(50);
     private int discount;
     Discount(int discount){
         this.discount = discount;
