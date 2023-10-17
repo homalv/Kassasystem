ca6666ba74839f7dac517a3904d13b118c1acb28
MagneticMojo <forsberg.bjorn.a@gmail.com>
2023-10-17 21:23:01 +0200

    Assortment and AssortmentTest refactored and moved to assortment package

---
+++ src/main/java/assortment/Assortment.java
@@ -0,0 +1,38 @@
+package assortment;
+
+import item.Item;
+import java.util.Map;
+import java.util.Optional;
+
+public class Assortment {
+
+    private final Map<String, Item> items;
+
+    public Assortment(Map<String, Item> items) {
+        this.items = items;
+    }
+
+    public int getNumberOfItemsInAssortment() {
+        return items.size();
+    }
+
+    public boolean contains(String ean) {
+        return items.containsKey(ean);
+    }
+
+    public Optional<Item> getItem(String ean) {
+        if (ean == null) {
+            throw new NullPointerException("EAN should not be null");
+        }
+
+        if (isInvalidEanFormat(ean)) {
+            throw new IllegalArgumentException("EAN must have 13 numerical characters");
+        }
+
+        return Optional.ofNullable(items.get(ean));
+    }
+
+    public boolean isInvalidEanFormat(String str) {
+        return !(str.length() == 13 && str.matches("^\\d+$"));
+    }
+}
---
+++ src/test/java/assortment/AssortmentTest.java
@@ -0,0 +1,69 @@
+package assortment;
+
+import assortment.Assortment;
+import item.Item;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AssortmentTest {
+
+    private static final String PINEAPPLE_EAN = "1234567890043";
+    private static final String NON_PRESENT_EAN = "0000000000001";
+    private static final String INCORRECT_EAN = "XX789WROMG";
+
+    private Assortment assortment;
+    private Item testItem;
+    private Map<String, Item> items;
+
+    @BeforeEach
+    void setUp() {
+        testItem = new Item("Pineapple", 20000L, PINEAPPLE_EAN);
+        items = new HashMap<>();
+        items.put(testItem.getEAN(), testItem);
+        assortment = new Assortment(items);
+    }
+
+    @Test
+    void testSize() {
+        assertEquals(1, assortment.getNumberOfItemsInAssortment());
+    }
+
+    @Test
+    void testContainsItem() {
+        assertTrue(assortment.contains(PINEAPPLE_EAN), "assortment does not contain specified argument");
+    }
+
+    @Test
+    void testNotContainsItem() {
+        assertFalse(assortment.contains(NON_PRESENT_EAN), "assortment contains argument it should not contain");
+    }
+
+    @Test
+    void testGetItem() {
+        Optional<Item> result = assortment.getItem(PINEAPPLE_EAN);
+        assertTrue(result.isPresent());
+        assertEquals(testItem, result.get());
+    }
+
+    @Test
+    void testGetItemNotPresent() {
+        Optional<Item> result = assortment.getItem(NON_PRESENT_EAN);
+        assertFalse(result.isPresent());
+    }
+
+    @Test
+    void testGetItemWithNullEAN() {
+        assertThrows(NullPointerException.class, () -> assortment.getItem(null));
+    }
+
+    @Test
+    void testGetItemWithIncorrectEAN() {
+        assertThrows(IllegalArgumentException.class, () -> assortment.getItem(INCORRECT_EAN));
+    }
+}
