20699e2a067f5bf469c27f7aaeadfae67067632b
MagneticMojo <forsberg.bjorn.a@gmail.com>
2023-10-17 23:38:09 +0200

    CSVParser and CSVParserTest added

---
+++ src/main/java/assortment/CSVParser.java
@@ -0,0 +1,40 @@
+package assortment;
+
+import item.Item;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CSVParser {
+
+    private static final int REQUIRED_CSV_PER_LINE = 4;
+
+    public Map<String, Item> parse(List<String> csvLines) {
+        Map<String, Item> itemsMap = new HashMap<>();
+
+        for (String line : csvLines) {
+            String[] parts = line.split(",");
+
+            if (parts.length != REQUIRED_CSV_PER_LINE) {
+                throw new ArrayIndexOutOfBoundsException("CSV line format is incorrect. Ensure each line has the correct number of fields");
+            }
+
+            String ean = parts[0];
+            String itemName = parts[1];
+            String category = parts[2]; // TODO needs to be implemented
+            long price;
+            try {
+                price = Long.parseLong(parts[3]);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("Invalid price format in CSV line: " + line, e);
+            }
+
+            Item item = new Item(itemName, price, ean);
+            itemsMap.put(ean, item);
+        }
+
+        return itemsMap;
+    }
+}
+
---
+++ src/test/java/assortment/CSVParserTest.java
@@ -0,0 +1,64 @@
+package assortment;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CSVParserTest {
+
+    private static final Path ASSORTMENT_RESOURCE_PATH = Path.of("test-assortment.csv");
+
+    public List<String> getLoadedCSVLines(Path pathToCsv) throws IOException {
+        return new CSVLoader().load(pathToCsv);
+    }
+
+    @Test
+    void testParsesWithoutErrors() {
+
+    }
+
+
+/*    @Test
+    void ctrThrowsForNonNumericalString() {
+        assertThrows(NumberFormatException.class, () -> new Assortment(NON_NUMERICAL_STRING_IN_CSV_PATH));
+    }
+
+    @Test
+    void ctrThrowsForInvalidCSVFormat() {
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> new Assortment(OUT_OF_BOUNDS_CSV_PATH));
+    }*/
+
+    /*
+     * EAN Code,item.Item Name,Category,Price
+     * 1234567890010,Sandwich,Food,8000
+     * */
+
+    /*
+     * production-assortment == test-assortment
+     * */
+
+    // Test with empty file
+    // items.txt
+
+    // Test with wrong file type
+
+    // Test each col?
+
+    // Test col in line in CSV is out of bounds
+    // oob.csv
+
+    // Test price string is non-numerical
+    // non-numerical-item-string.csv
+
+    // Exception e = assertThrows()
+    // + Check message
+
+    // String s = assertTimeout()
+    // + additional checks on s
+
+
+}
