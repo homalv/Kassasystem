7626212179e9e8f883c032521b242410c97f3309
MagneticMojo <forsberg.bjorn.a@gmail.com>
2023-10-17 14:40:16 +0200

    AssortmentTest updated with JUnit5 annotations and assertTimeout()

--- src/test/java/AssortmentTest.java
+++ src/test/java/AssortmentTest.java
@@ -1,12 +1,19 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.suite.api.SuiteDisplayName;
 
+import static java.time.Duration.ofMillis;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SuiteDisplayName("Assortment Test Suite")
 class AssortmentTest {
 
     // TODO --> USE JUnit CSV
     // TODO --> Use Path
+    // TODO --> CTR faster
 
+    private static final int CTR_EXE_TIME_THRESHOLD = 0;
     private static final String ASSORTMENT_RESOURCE_PATH = "/test-assortment.csv";
     private static final String NON_CSV_FILE_PATH = "/items.txt";
     private static final String INVALID_PATH = "/invalid/invalid.csv";
@@ -14,6 +21,29 @@ class AssortmentTest {
     private static final String OUT_OF_BOUNDS_CSV_PATH = "/oob.csv";
     private static final String PINEAPPLE_EAN = "1234567890017";
     private final Item pineapple = new Item("Pineapple",200);
+
+    @Test
+    @Disabled("Test for displaying the disabled annotation")
+    void skippedTest() {
+        // Empty
+    }
+
+    @Test
+    @DisplayName("Ctr execution speed test")
+    void testCtrSpeedBelowThreshold() {
+        assertTimeout(ofMillis(CTR_EXE_TIME_THRESHOLD), () -> {
+            Assortment assortment = new Assortment(ASSORTMENT_RESOURCE_PATH);
+        }, "Ctr executing exceeded accepted threshold");
+    }
+
+    @Test
+    @DisplayName("Ctr exe speed test - abort preemptively")
+    void testCtrSpeedBelowThresholdAbortPreemptively() {
+        assertTimeoutPreemptively(ofMillis(CTR_EXE_TIME_THRESHOLD), () -> {
+            Assortment assortment = new Assortment(ASSORTMENT_RESOURCE_PATH);
+        }, "Ctr executing exceeded accepted threshold - abort exe preemptively");
+    }
+
 
     @Test
     void ctrThrowsForNonCsvFile() {
@@ -38,12 +68,12 @@ class AssortmentTest {
     @Test
     void ctrPopulatesAssortment() {
         Assortment assortment = new Assortment(ASSORTMENT_RESOURCE_PATH);
-        assertEquals(37, assortment.getAssortmentSize());
+        assertEquals(37, assortment.getAssortmentSize(), "Num of lines csv does not match expectations");
     }
 
     @Test
     void testAssortmentContainsItem() {
         Assortment assortment = new Assortment(ASSORTMENT_RESOURCE_PATH);
-        assertTrue(assortment.contains(PINEAPPLE_EAN));
+        assertTrue(assortment.contains(PINEAPPLE_EAN), "Assortment does not contain specified argument");
     }
 }
