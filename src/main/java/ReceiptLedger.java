b9eaee1cd80d077ba7eb749fbb02965a52438f1c
nidorbRotkiv <viktor.karl.brodin.1@gmail.com>
2023-10-26 14:46:02 +0200

    Changed ReceiptLedger to use data structures instead of a textfile.

--- src/main/java/ReceiptLedger.java
+++ src/main/java/ReceiptLedger.java
@@ -1,13 +1,10 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 public class ReceiptLedger implements ReceiptRepository {
-    private List<Receipt> receipts = new ArrayList<>();
-    // should use the real file instead of temp, but i have no access atm. TODO
-    private static String RECEIPT_LEDGER_RESOURCE_PATH = System.getProperty("java.io.tmpdir") + "/receipt-ledger.txt";
+    private Set<Receipt> receipts = new HashSet<>();
 
     public ReceiptLedger() {
 
@@ -15,20 +12,11 @@ public class ReceiptLedger implements ReceiptRepository {
 
     @Override
     public boolean add(Receipt receiptToLog) {
-        try {
-            FileWriter writer = new FileWriter(RECEIPT_LEDGER_RESOURCE_PATH, true);
-            String serializedReceipt = serializeReceipt(receiptToLog);
-            writer.write(serializedReceipt);
-            writer.write(System.lineSeparator());
-            writer.close();
-            return true;
-        } catch (IOException e) {
+        if(receipts.contains(receiptToLog) || receiptToLog == null) {
             return false;
         }
-    }
-
-    private String serializeReceipt(Receipt receipt) {
-        return receipt.getSerialNumber();
+        receipts.add(receiptToLog);
+        return true;
     }
 
     @Override
--- src/test/java/ReceiptLedgerTest.java
+++ src/test/java/ReceiptLedgerTest.java
@@ -1,47 +1,49 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Map;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-// TODO: Jag har kopierat mycket från Register här, kan vi undvika?
-
-class ReceiptLedgerTest {
-
-    private static final Path ASSORTMENT_RESOURCE_PATH = Path.of("assortment.csv");
+public class ReceiptLedgerTest {
+    private static final Path ASSORTMENT_RESOURCE_PATH = Path.of("test-assortment.csv");
     private static final String PINEAPPLE_EAN = "1234567890017";
     private Register registerWithInitPurchase;
-    private ReceiptLedger ledger;
-    private Map<String, Item> items;
+    @Mock
+    private ReceiptLedger receiptLedger;
+    private Receipt receipt;
 
     @BeforeEach
-    void setUp() throws IOException {
-        ledger = new ReceiptLedger();
-        CSVLoader loader = new CSVLoader();
-        CSVParser parser = new CSVParser();
-        items = parser.parse(loader.load(ASSORTMENT_RESOURCE_PATH));
-        Assortment assortment = new Assortment(items);
-        registerWithInitPurchase = new Register(assortment, ledger);
+    void setUp() {
+        Assortment assortment = AssortmentFactory.createAssortment(ASSORTMENT_RESOURCE_PATH);
+        registerWithInitPurchase = new Register(assortment, receiptLedger);
+        MockitoAnnotations.initMocks(this);
         registerWithInitPurchase.initializePurchase();
+        receipt = getReceiptWithOneItem();
     }
 
-    public Register getRegisterWithCartWithOneAddedItem() {
-        Assortment assortment2 = new Assortment(items);
-        Register registerWithInitPurchase = new Register(assortment2, ledger);
+    public Receipt getReceiptWithOneItem() {
         registerWithInitPurchase.initializePurchase();
         registerWithInitPurchase.addToCart(PINEAPPLE_EAN);
-        return registerWithInitPurchase;
+        return registerWithInitPurchase.createReceipt();
     }
 
     @Test
-    void addReceiptToLedger() {
-       Receipt r = getRegisterWithCartWithOneAddedItem().createReceipt();
-       assertTrue(ledger.add(r));
-       //var allReceipts = ledger.getAllReceipts();
-       //assertEquals(allReceipts.get(allReceipts.size()-1), r);
+    void testAddReceipt() {
+        when(receiptLedger.add(any(Receipt.class))).thenReturn(true);
+        boolean result = receiptLedger.add(receipt);
+        assertTrue(result);
     }
+
+    @Test
+    void testAddDuplicateReceipt() {
+        when(receiptLedger.add(any(Receipt.class))).thenReturn(false);
+        boolean result = receiptLedger.add(receipt);
+        assertFalse(result);
+    }
+
 }
